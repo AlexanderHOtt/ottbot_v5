3202f1f50ab65f557948c28ffc7730d0cb835b7c
AlexanderHOtt <aott01@protonmail.ch>
2022-07-11 00:32:43 -0700

    refactor messy music code

--- ottbot/modules/slash_commands/voice/music.py
+++ ottbot/modules/slash_commands/voice/music.py
@@ -1,12 +1,93 @@
 # -*- coding=utf-8 -*-
 """Music related slash commands."""
+import logging
 import typing
 
 import hikari
 import lavasnek_rs
 import tanjun
 
+from ottbot import config as config_
+
 music = tanjun.Component()
+
+logger = logging.getLogger(__name__)
+
+
+class EventHandler:
+    """Handles events from the Lavalink server."""
+
+    async def track_start(self, _: lavasnek_rs.Lavalink, event: lavasnek_rs.TrackStart) -> None:
+        """Handles track start events."""
+        logger.info(f"Track started on guild: {event.guild_id}")
+
+    async def track_finish(self, _: lavasnek_rs.Lavalink, event: lavasnek_rs.TrackFinish) -> None:
+        """Handles track finish events."""
+        logger.info(f"Track finished on guild: {event.guild_id}")
+
+    async def track_exception(self, lavalink: lavasnek_rs.Lavalink, event: lavasnek_rs.TrackException) -> None:
+        """Handles track exception events."""
+        logger.info(f"Track exception event happened on guild: {event.guild_id}")
+
+        # If a track was unable to be played, skip it
+        skip = await lavalink.skip(event.guild_id)
+        node = await lavalink.get_guild_node(event.guild_id)
+
+        if skip and node:
+            if not node.queue and not node.now_playing:
+                await lavalink.stop(event.guild_id)
+
+
+@music.with_listener(hikari.ShardReadyEvent)
+async def on_shard_ready(
+    event: hikari.ShardReadyEvent,
+    client_: tanjun.Client = tanjun.injected(type=tanjun.Client),
+    config: config_.FullConfig = tanjun.inject(type=config_.FullConfig),
+) -> None:
+    """Event that triggers when the hikari gateway is ready."""
+    if config.lavalink_password is None:
+        return
+    builder = (
+        lavasnek_rs.LavalinkBuilder(event.my_user.id, config.tokens.bot)
+        .set_host(config.lavalink_host)
+        .set_password(config.lavalink_password)
+        .set_start_gateway(False)
+        # We set start gateway False because hikari can handle
+        # voice events for us.
+    )
+
+    # Here we add lavasnek_rs.Lavalink as a type dependency to the client
+    # We will use this later to have access to it in all our commands
+
+    client_.set_type_dependency(lavasnek_rs.Lavalink, await builder.build(EventHandler))
+
+
+@music.with_listener(hikari.VoiceStateUpdateEvent)
+async def on_voice_state_update(
+    event: hikari.VoiceStateUpdateEvent,
+    lavalink: lavasnek_rs.Lavalink = tanjun.injected(type=lavasnek_rs.Lavalink),
+) -> None:
+    """Passes voice state updates to lavalink."""
+    lavalink.raw_handle_event_voice_state_update(
+        event.state.guild_id,
+        event.state.user_id,
+        event.state.session_id,
+        event.state.channel_id,
+    )
+
+
+@music.with_listener(hikari.VoiceServerUpdateEvent)
+async def on_voice_server_update(
+    event: hikari.VoiceServerUpdateEvent,
+    lavalink: lavasnek_rs.Lavalink = tanjun.injected(type=lavasnek_rs.Lavalink),
+) -> None:
+    """Passes voice server updates to lavalink."""
+    if event.endpoint is not None:
+        await lavalink.raw_handle_event_voice_server_update(
+            event.guild_id,
+            event.endpoint,
+            event.token,
+        )
 
 
 @music.with_slash_command
