1f1ecf8528c70317f5ec21df27761cc59f8f4ccf
AlexanderHOtt <aott01@protonmail.ch>
2022-07-22 18:11:10 -0700

    feat: add start screen

--- ottbot/__main__.py
+++ ottbot/__main__.py
@@ -1,6 +1,7 @@
 # -*- coding=utf-8 -*-
 """Main entry point for module cli."""
 import os
+import typing as t
 
 import dotenv
 
@@ -8,6 +9,19 @@ from ottbot import bot
 
 dotenv.load_dotenv()
 
+
+VERSION: t.Final[str] = "5.0.0 alpha 1"
+# logging is handled by hikari, but hikari isn't initialized until `bot.run()` is called
+print(  # noqa: T001
+    rf"""
+  _______             ______                      _______     OttBot v{VERSION}
+ (_______)  _     _  (____  \        _           (_______)
+  _     _ _| |_ _| |_ ____)  ) ___ _| |_    _   _ ______      GitHub:  https://github.com/AlexanderHOtt/ottbot_v5
+ | |   | (_   _|_   _)  __  ( / _ (_   _)  | | | (_____ \     Discord: ...
+ | |___| | | |_  | |_| |__)  ) |_| || |_    \ V / _____) )
+  \_____/   \__)  \__)______/ \___/  \__)    \_/ (______/
+"""
+)
 
 if __name__ == "__main__":
     if os.name != "nt":
